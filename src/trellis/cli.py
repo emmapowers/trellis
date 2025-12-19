a9eaf0b9c33bc91f0ae4b6e48568bbe10cf83fbf
Emma Powers <emma@emmaponders.com>
2025-12-18 21:53:21 -0500

    Make platform imports conditional in CLI bundle command

--- src/trellis/cli.py
+++ src/trellis/cli.py
@@ -23,13 +23,15 @@ def bundle() -> None:
 )
 def build(force: bool, platform: str) -> None:
     """Build platform bundles."""
-    from trellis.platforms.browser.serve_platform import BrowserServePlatform
-    from trellis.platforms.desktop.platform import DesktopPlatform
-    from trellis.platforms.server.platform import ServerPlatform
-
     if platform in ("server", "all"):
+        from trellis.platforms.server.platform import ServerPlatform
+
         ServerPlatform().bundle(force=force)
     if platform in ("desktop", "all"):
+        from trellis.platforms.desktop.platform import DesktopPlatform
+
         DesktopPlatform().bundle(force=force)
     if platform in ("browser", "all"):
+        from trellis.platforms.browser.serve_platform import BrowserServePlatform
+
         BrowserServePlatform().bundle(force=force)
